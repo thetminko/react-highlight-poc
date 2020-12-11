14aaaea2610ce0a9a030a92d8c776a21fbe04657
minko <minko.thet@ufinity.com>
2020-12-11 09:43:55 +0800

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import parse, { DomElement, domToReact, HTMLReactParserOptions } from 'html-react-parser';
 import 'tinymce/tinymce';
 import 'tinymce/icons/default';
@@ -13,7 +13,7 @@ import 'tinymce/skins/content/default/content.min.css';
 import { Editor } from '@tinymce/tinymce-react';
 
 function App() {
-  const [paragraph, setParagrah] = useState('<p id="highlight-main">Hello world <span id="replace" class="bg-purple-500 text-white cursor-pointer" uuid="abcded" any="test">Here should be sth</span></p>');
+  const [paragraph, setParagrah] = useState('Hello world <span id="replace" class="bg-purple-500 text-white cursor-pointer" data-uuid="abcded" any="test">Here should be sth</span>');
 
   const [selectedText, setSelectedText] = useState('');
 
@@ -33,49 +33,56 @@ function App() {
           return <span></span>;
         }
 
-        return <span className="bg-purple-500 text-white cursor-pointer" onClick={() => onClick(attribs.uuid)}>{domToReact(children, options)}</span>
-      }
-
-      if (name === 'p' && attribs.id === "highlight-main") {
-        if (children) {
-          return <p id="highlight-main" onMouseUp={onSelectText}>{domToReact(children, options)}</p>
-        }
-
-        return <p></p>
+        return <span id="replace" className="bg-purple-500 text-white cursor-pointer" data-uuid={attribs['data-uuid']} onClick={() => onClick(attribs['data-uuid'])}>{domToReact(children, options)}</span>
       }
     }
   }
+
+  useEffect(() => {
+    console.log('State: ', paragraph);
+  }, [paragraph])
 
   const handleEditorChange = (content: string, editor: any) => {
     console.log('Content was updated:', content);
     setParagrah(content);
   }
 
-  const onSelectText = () => {
+  const onSelectText = (e: React.MouseEvent<HTMLParagraphElement, MouseEvent>) => {
+    console.log(e);
+
     const selection = window.getSelection();
     const selectedText = selection?.toString();
     if (selectedText) {
       setSelectedText(selectedText);
       const range = selection?.getRangeAt(0);
+
+      if (!range) {
+        return;
+      }
+
       const surroundingElement = document.createElement('span');
-      surroundingElement.id= "replace";
+      surroundingElement.id = 'replace';
       surroundingElement.className = "bg-purple-500 text-white cursor-pointer";
-      surroundingElement.setAttribute('uuid', "abcded");
+      surroundingElement.setAttribute('data-uuid', 'new_uuid');
+      surroundingElement.onclick = () => onClick('new_uuid');
       range?.surroundContents(surroundingElement);
+      const html = e.currentTarget.innerHTML;
+      setTimeout(() => {
+        setParagrah(html);
+      }, 1000);
     } else {
       setSelectedText('');
     }
   }
 
-  const markAsComment = () => {
-
-  }
-
   return (
     <div className="App">
      <div>
-       {parse(paragraph, options)}
+       <h3 className="font-bold">For display</h3>
+        <p onMouseUp={e => onSelectText(e)}>{parse(paragraph, options)}</p>
         <br />
+
+        <h3 className="font-bold">For editing</h3>
         <Editor initialValue={paragraph}
         init={{
           extended_valid_elements: "span[id|uuid|class|className|any]", 
@@ -86,9 +93,6 @@ function App() {
         onEditorChange={handleEditorChange} />
      </div>
      <p className="mt-4">You selected: {selectedText}</p>
-     <button className="mt-4 bg-blue-500 p-4" onClick={markAsComment}>
-       Mark as comment
-     </button>
     </div>
   );
 }
